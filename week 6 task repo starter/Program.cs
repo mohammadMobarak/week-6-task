94e6222e798eaf97127d8715407ae7f587d15327
Mohammad Mobarak <mohammad2632002mobarak@gmail.com>
2026-08-22 21:28:35 +0300

    Add project files.

---
+++ week 6 task repo starter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace week_6_task_repo_starter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Bashmohandes Ahmed !! ");
+        }
+    }
+}
